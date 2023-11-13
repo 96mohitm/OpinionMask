28b81322425f6d40cf7f9baf01fce96443fe4387
Mohit Musaddi <96mohitm@gmail.com>
2023-11-13 17:55:15 +0530

    added auth middleware in backend.

--- opinionmask_api/settings.py
+++ opinionmask_api/settings.py
@@ -79,6 +79,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'token_middleware.TokenAuthMiddleware',
 ]
 
 REST_FRAMEWORK = {
