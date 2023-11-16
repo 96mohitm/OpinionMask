325cc1fa5673d0d878b6735fc43b378fde7592de
Mohit Musaddi <96mohitm@gmail.com>
2023-11-16 11:43:53 +0530

    Refactored PostPage component.

--- frontend/src/components/posts/PostPage.tsx
+++ frontend/src/components/posts/PostPage.tsx
@@ -75,6 +75,50 @@ const PostPage = () => {
     setAnonFilter(event.target.value);
   };
 
+  const postHeader = () => {
+    return (
+      <div>
+        <div className="text-left mb-4">
+          <div className="font-medium text-[28px] tracking-[0] leading-[normal]">
+            Hello {username}
+          </div>
+        </div>
+        <div className="text-left mb-4">
+          <p className="font-normal text-[#7f8084] text-[16px] tracking-[0] leading-[24px]">
+            How are you doing today? Would you like to share something with the community 🤗
+          </p>
+        </div>
+      </div>
+    )
+  };
+
+  const postFilter = () => {
+    return (
+      <div className="mb-4">
+        <div className='flex justify-between'>
+          <select
+            className="mt-1 block w-32 py-2 px-2 border border-black bg-[#27292d] text-[#7f8084] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-white focus:border-white"
+            value={myPost ? 'my' : 'all'}
+            onChange={handleFilterChange}
+          >
+            <option value="my">My Posts</option>
+            <option value="all">My feed</option>
+          </select>
+
+          <select
+            className="mt-1 block w-48 py-2 px-2 border border-black bg-[#27292d] text-[#7f8084] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-white focus:border-white"
+            value={anonFilter}
+            onChange={handleAnonFilterChange}
+          >
+            <option value="ALL">All Posts</option>
+            <option value="NON_ANON">Non Anonymous Post</option>
+            <option value="ANON">Anonymous Posts</option>
+          </select>
+        </div>
+      </div>
+    );
+  };
+
   if (loading) return (
     <h2> loading...</h2>
   );
@@ -83,41 +127,9 @@ const PostPage = () => {
     <div className="bg-primary text-white min-h-screen flex justify-center">
       <div className="flex flex-col md:w-[700px] gap-10">
         {/* Content goes here */}
-        <div>
-          <div className="text-left mb-4">
-            <div className="font-medium text-[28px] tracking-[0] leading-[normal]">
-              Hello {username}
-            </div>
-          </div>
-          <div className="text-left mb-4">
-            <p className="font-normal text-[#7f8084] text-[16px] tracking-[0] leading-[24px]">
-              How are you doing today? Would you like to share something with the community 🤗
-            </p>
-          </div>
-        </div>
+        { postHeader() }
         <CreatePostForm fetchData={fetchData} />
-        <div className="mb-4">
-          <div className='flex justify-between'>
-            <select
-              className="mt-1 block w-32 py-2 px-2 border border-black bg-[#27292d] text-[#7f8084] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-white focus:border-white"
-              value={myPost ? 'my' : 'all'}
-              onChange={handleFilterChange}
-            >
-              <option value="my">My Posts</option>
-              <option value="all">My feed</option>
-            </select>
-
-            <select
-              className="mt-1 block w-48 py-2 px-2 border border-black bg-[#27292d] text-[#7f8084] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-white focus:border-white"
-              value={anonFilter}
-              onChange={handleAnonFilterChange}
-            >
-              <option value="ALL">All Posts</option>
-              <option value="NON_ANON">Non Anonymous Post</option>
-              <option value="ANON">Anonymous Posts</option>
-            </select>
-          </div>
-        </div>
+        { postFilter() }
         {
           isDataLoading ?
             <div className='flex justify-center'> <Spinner /> </div>
